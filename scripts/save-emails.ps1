2a7e836b38319cf68a4b4cb98b74d976aa2229e3
Lada Kesseler <archie.kesseler@gmail.com>
2025-07-30 10:38:22 -0600

    Fix Windows file locking and PowerShell collection modification bugs

--- scripts/save-emails.ps1
+++ scripts/save-emails.ps1
@@ -3,7 +3,14 @@
 $outlook = New-Object -ComObject Outlook.Application
 $folder = $outlook.GetNamespace("MAPI").GetDefaultFolder(6).Folders("PCIT Automatic")
 
+# Copy items to array first to avoid collection modification during iteration
+$mailItems = @()
 foreach($mail in $folder.Items) {
+    $mailItems += $mail
+}
+
+# Now process the copied items
+foreach($mail in $mailItems) {
     $filename = "C:\Users\wilsora3\Documents\PCIT Automated\$($mail.ReceivedTime.ToString('yyyyMMdd_HHmmss')).msg"
     $mail.SaveAs($filename, 3)  # 3 = MSG format
     $mail.Move($outlook.GetNamespace("MAPI").GetDefaultFolder(6).Folders("PCIT-Processed"))
