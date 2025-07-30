264a0c8b2feab2119dc0dc72e2a15b1904ce9de6
Lada Kesseler <archie.kesseler@gmail.com>
2025-07-30 12:00:19 -0600

    Better logging

--- scripts/save-emails.ps1
+++ scripts/save-emails.ps1
@@ -2,14 +2,17 @@
 
 $outlook = New-Object -ComObject Outlook.Application
 $namespace = $outlook.GetNamespace("MAPI")
-$sourceFolder = $namespace.GetDefaultFolder(6).Folders("PCIT Automatic")
-$processedFolder = $namespace.GetDefaultFolder(6).Folders("PCIT-Processed")
+$inboxFolderId = 6
+$sourceFolder = $namespace.GetDefaultFolder($inboxFolderId).Folders("PCIT Automatic")
+$processedFolder = $namespace.GetDefaultFolder($inboxFolderId).Folders("PCIT-Processed")
 
 # Collect EntryIDs first to avoid collection modification during iteration
 $entryIds = @()
 foreach($mail in $sourceFolder.Items) {
     $entryIds += $mail.EntryID
 }
+
+Write-Host "Found $($entryIds.Count) emails to process"
 
 # Process each email by EntryID
 foreach($entryId in $entryIds) {
