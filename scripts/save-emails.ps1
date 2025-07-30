6c9da2c791dd9a97ce9bcb6c176f56e92b4ff308
Lada Kesseler <archie.kesseler@gmail.com>
2025-07-30 11:57:14 -0600

    Fix

--- scripts/save-emails.ps1
+++ scripts/save-emails.ps1
@@ -1,18 +1,17 @@
 # Export emails from Outlook folder to .msg files
 
 $outlook = New-Object -ComObject Outlook.Application
-$folder = $outlook.GetNamespace("MAPI").GetDefaultFolder(6).Folders("PCIT Automatic")
+$namespace = $outlook.GetNamespace("MAPI")
+$sourceFolder = $namespace.GetDefaultFolder(6).Folders("PCIT Automatic")
+$processedFolder = $namespace.GetDefaultFolder(6).Folders("PCIT-Processed")
 
 # Collect EntryIDs first to avoid collection modification during iteration
 $entryIds = @()
-foreach($mail in $folder.Items) {
+foreach($mail in $sourceFolder.Items) {
     $entryIds += $mail.EntryID
 }
 
-# Now process each email by looking it up by EntryID
-$namespace = $outlook.GetNamespace("MAPI")
-$processedFolder = $namespace.GetDefaultFolder(6).Folders("PCIT-Processed")
-
+# Process each email by EntryID
 foreach($entryId in $entryIds) {
     $mail = $namespace.GetItemFromID($entryId)
     $filename = "C:\Users\wilsora3\Documents\PCIT Automated\$($mail.ReceivedTime.ToString('yyyyMMdd_HHmmss')).msg"
